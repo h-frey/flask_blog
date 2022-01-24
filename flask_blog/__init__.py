f839be5e0a1a0181d8273d98ce6e8197c9dbfd66
Humphrey <hnyanzi32@gmail.com>
2022-01-24 22:49:21 +0300

    Initial Commit

---
+++ flask_blog/__init__.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SECRET_KEY"] = "5b4089adff7f5a5c305cda090f6d05f5"
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view="login"
+login_manager.login_message_category="info"
+
+
+from flask_blog import routes
---
+++ flask_blog/forms.py
@@ -0,0 +1,68 @@
+
+from flask_wtf import Form
+from flask_wtf.file import FileField,FileAllowed
+from flask_login import current_user
+from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField,validators
+from wtforms.validators import InputRequired,Email,ValidationError
+from flask_blog.models import User
+
+
+class RegistrationForm(Form):
+    username = StringField("Username", [InputRequired("Please enter your name")])
+    email = StringField("Email",[InputRequired("Please enter your email adress"),Email("This field requires a valid email adress ")])
+    password = PasswordField("Password", [InputRequired()])
+    confirm_password = PasswordField("Confirm Password", [InputRequired(),validators.EqualTo("password",message="Passwords must match")])
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if  user:
+            raise ValidationError("That username is taken,please use another one")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                "That email is taken,please use another one")
+
+class LoginForm(Form):
+    
+    email = StringField("Email", [InputRequired("Please enter your name")])
+
+    password = PasswordField("Password", [InputRequired("Please enter your name")])
+
+    remember = BooleanField("Remember Me")
+
+    submit = SubmitField("Login")
+
+
+class UpdateAccountForm(Form):
+    
+    username = StringField(
+        "Username", [InputRequired("Please enter your name")])
+
+    email = StringField("Email", [InputRequired("Please enter your email adress"), Email(
+        "This field requires a valid email adress ")])
+
+    picture = FileField("Update Profile Picture")
+
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    "That username is taken,please use another one")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    "That email is taken,please use another one")
+
+class PostForm(Form):
+    title = StringField("Title",validators = [InputRequired()])
+    content = TextAreaField("Content", [InputRequired()])
+    submit = SubmitField("Post")
