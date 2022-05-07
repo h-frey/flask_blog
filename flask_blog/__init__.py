429bf81612676a81bf21e54285bd93539905dc9c
Humphrey <hnyanzi32@gmail.com>
2022-05-08 00:29:11 +0300

    First Time LInux Changes

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,7 +1,7 @@
 from flask_mail import Mail
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 
@@ -13,7 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 app.config["SECRET_KEY"] = "5b4089adff7f5a5c305cda090f6d05f5"
 app.config["UPLOAD_FOLDER"]= UPLOAD_FOLDER
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+# bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view="login"
 login_manager.login_message_category="info"
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,9 +1,10 @@
 from ast import Return
 
 import os
-from PIL import Image as Im
+# from PIL import Image as Im
 from flask import  render_template, url_for, flash, redirect, request,abort
-from flask_blog import app, db, bcrypt,ALLOWED_EXTENSIONS,mail
+from flask_blog import app, db,ALLOWED_EXTENSIONS,mail
+# from flask_blog import bcrypt
 from flask_blog.forms import RegistrationForm, LoginForm, PostForm,UpdateAccountForm,ResetPasswordForm,RequestResetForm
 from flask_blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -31,8 +32,9 @@ def register():
     form = RegistrationForm(request.form)
 
     if request.method == "POST" and form.validate():
-        hashed_password = bcrypt.generate_password_hash(
-            form.password.data).decode("utf-8")
+        # hashed_password = bcrypt.generate_password_hash(
+            # form.password.data).decode("utf-8")
+        hashed_password = form.password.data
         user = User(username=form.username.data,
                     email=form.email.data, password=hashed_password)
         db.session.add(user)
@@ -50,7 +52,8 @@ def login():
     form = LoginForm(request.form)
     if request.method == "POST" and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user:
+        # if bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get("next")
             return redirect(next_page) if next_page else redirect(url_for("home"))
